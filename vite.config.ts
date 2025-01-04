a567049e18888088b3c71a243b02932c606058c7
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-05 00:09:42 +0700

    Add Docker support with development and production configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  preview: {
+    port: 8000,
+    strictPort: true,
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 7001,
+  },
 });
