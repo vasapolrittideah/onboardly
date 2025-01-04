8631aa05b2c1cac62d6761c13ca4147a01265cbf
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-04 16:44:37 +0700

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSameLine: true,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
+
+export default config;
