d34323a96beb720ba34a3db4fa9ecee1eb9c77bf
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-08 17:25:36 +0000

    Set up NestJS app for backend development with initial configuration and modules

--- packages/server/src/database/schema/column.helpers.ts
+++
@@ -1,7 +0,0 @@
-import { timestamp } from 'drizzle-orm/pg-core';
-
-export const timestamps = {
-  updated_at: timestamp(),
-  created_at: timestamp().defaultNow().notNull(),
-  deleted_at: timestamp(),
-};
