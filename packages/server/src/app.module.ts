974efdba8a7c7a6ddd0ea25fd169a87a5099641e
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-09 02:07:22 +0700

    Add Users module, controller, and service as a foundation for handling user-related operations

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
+  imports: [UsersModule],
 })
 export class AppModule {}
