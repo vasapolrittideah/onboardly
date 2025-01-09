fb263644c5fd99c7dea44a815483b14b85ab9bfc
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-09 09:39:08 +0700

    Set up Swagger documentation and switch to Express adapter due to Fastify v5 incompatibility

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,12 +4,26 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new ExpressAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
