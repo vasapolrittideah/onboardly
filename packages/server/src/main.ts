0ec20a97dda93aa3d2629129434777bd2945123e
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-09 23:40:54 +0700

    Add Webpack configuration and enable Hot Module Replacement for development

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,6 +6,8 @@ import {
   NestExpressApplication,
 } from '@nestjs/platform-express';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
@@ -21,5 +23,10 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
