ac8351b69d076fa01b1e7ad6603fdf89d0960174
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-01-20 20:43:09 +0700

    Run Prettier to automatically sort Tailwind CSS classes

--- packages/web/src/routes/sign-up.lazy.tsx
+++ packages/web/src/routes/sign-up.lazy.tsx
@@ -25,11 +25,11 @@ function RouteComponent() {
 
   return (
     <div className="flex w-full flex-col items-center justify-center pt-24">
-      <div className="flex size-14 items-center justify-center rounded-full border border-stroke-soft-200 bg-white shadow-regular-sm">
+      <div className="border-stroke-soft-200 shadow-regular-sm flex size-14 items-center justify-center rounded-full border bg-white">
         <RiUserAddLine className="text-gray-800" />
       </div>
-      <h1 className="mt-6 text-title-h4">Create a new account.</h1>
-      <p className="mt-2 text-center text-paragraph-md text-gray-600">
+      <h1 className="text-title-h4 mt-6">Create a new account.</h1>
+      <p className="text-paragraph-md mt-2 text-center text-gray-600">
         Enter your credentials to register and start your journey.
       </p>
       <Divider.Root
@@ -37,7 +37,7 @@ function RouteComponent() {
           background:
             'linear-gradient(90deg, currentcolor 4px, transparent 4px) 50% 50% / 8px 1px repeat-x',
         }}
-        className="mt-8 h-1 w-full max-w-[440px] text-stroke-sub-300 before:bg-transparent"></Divider.Root>
+        className="text-stroke-sub-300 mt-8 h-1 w-full max-w-[440px] before:bg-transparent"></Divider.Root>
 
       <div className="mt-4 h-80 w-full max-w-[440px] rounded-3xl bg-white p-6">
         <div className="flex flex-col gap-1">
@@ -75,9 +75,9 @@ function RouteComponent() {
               />
               <button type="button" onClick={() => setShowPassword((s) => !s)}>
                 {showPassword ? (
-                  <RiEyeOffLine className="size-5 text-text-soft-400 group-has-[disabled]:text-text-disabled-300" />
+                  <RiEyeOffLine className="text-text-soft-400 group-has-[disabled]:text-text-disabled-300 size-5" />
                 ) : (
-                  <RiEyeLine className="size-5 text-text-soft-400 group-has-[disabled]:text-text-disabled-300" />
+                  <RiEyeLine className="text-text-soft-400 group-has-[disabled]:text-text-disabled-300 size-5" />
                 )}
               </button>
             </Input.Wrapper>
@@ -92,7 +92,7 @@ function RouteComponent() {
           Login
         </FancyButton.Root>
 
-        <div className="mt-4 flex justify-center gap-1 text-paragraph-sm text-text-sub-600">
+        <div className="text-paragraph-sm text-text-sub-600 mt-4 flex justify-center gap-1">
           Already have and account?
           <Link to="/login">
             <LinkButton.Root variant="gray">
